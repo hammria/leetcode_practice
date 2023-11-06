0a0392d4d4aaaf63ac8e09fc989803a7fa763817
hammria <chinonericardo@outlook.com>
2023-11-06 22:42:22 +0800

    day 15

---
+++ linkedlist/odd_even_list.go
@@ -0,0 +1,18 @@
+package linkedlist
+
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil || head.Next.Next == nil {
+		return head
+	}
+	odd := head
+	even := head.Next
+	evenHead := even
+	for even != nil && even.Next != nil {
+		odd.Next = even.Next
+		odd = odd.Next
+		even.Next = odd.Next
+		even = even.Next
+	}
+	odd.Next = evenHead
+	return head
+}
